8d88a5363a72c72b09a998484c2bde54fe180fcd
Pe4enka9 <ivan.tixonov000@gmail.com>
2026-06-08 00:11:19 +0700

    авторизация

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,12 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Session\Middleware\StartSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,6 +17,12 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
+
+        $middleware->appendToGroup('api', [
+            EncryptCookies::class,
+            AddQueuedCookiesToResponse::class,
+            StartSession::class,
+        ]);
 
         $middleware->alias([
             'admin' => AdminMiddleware::class,
