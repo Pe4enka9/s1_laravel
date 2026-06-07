7304001c98c906ba7012de79d66670e5b82e9750
Pe4enka9 <ivan.tixonov000@gmail.com>
2026-06-08 02:06:53 +0700

    авторизация

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -11,7 +11,7 @@ class AdminMiddleware
 {
     public function handle(Request $request, Closure $next): Response
     {
-        if (!$request->user() || !$request->user()->is_admin) {
+        if (!Auth::check() || !Auth::user()->is_admin) {
             return response()->json([], 403);
         }
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,9 @@
 <?php
 
 use App\Http\Middleware\AdminMiddleware;
-use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
-use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Session\Middleware\StartSession;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -17,12 +14,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->statefulApi();
-
-        $middleware->appendToGroup('api', [
-            EncryptCookies::class,
-            AddQueuedCookiesToResponse::class,
-            StartSession::class,
-        ]);
 
         $middleware->alias([
             'admin' => AdminMiddleware::class,
